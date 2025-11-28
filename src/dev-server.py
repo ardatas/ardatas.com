5fa4bc2e7f33faa766ae315cc3d09611ea952afc
Sagar Patil <sagarreddypatil@gmail.com>
2025-11-28 01:30:48 -0800

    remove marching cubes

--- src/dev-server.py
+++ src/dev-server.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, send_from_directory
 
-dist = os.path.join(os.getcwd(), 'dist')
-public = os.path.join(os.getcwd(), 'public')
+dist = os.path.join(os.getcwd(), "dist")
+public = os.path.join(os.getcwd(), "public")
 
 app = Flask(__name__)
 
-@app.route('/<path:path>')
+
+@app.route("/<path:path>")
 def send_static(path):
     try:
         return send_from_directory(dist, path, max_age=0)
@@ -14,12 +15,13 @@ def send_static(path):
         try:
             return send_from_directory(dist, f"{path}.html", max_age=0)
         except:
-            return send_from_directory(public, path, max_age=60*60*24*365)
+            return send_from_directory(public, path, max_age=60 * 60 * 24 * 365)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return send_from_directory(dist, 'index.html', max_age=0)
+    return send_from_directory(dist, "index.html", max_age=0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=5000)
